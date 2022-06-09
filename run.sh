a422ef596d6009e200ef64c6d1564b0731d868d3
Darius Foo <darius.foo.tw@gmail.com>
2022-06-09 15:46:08 +0800

    Update launch script

--- run.sh
+++ run.sh
@@ -24,15 +24,26 @@ make prepare
 # | cl | c  |
 # -----------
 
-tmux new-session -d -s test_2pc $SHELL
-tmux send-keys "$p2" ENTER
-tmux split-window -h
-tmux send-keys "$p1" ENTER
-tmux split-window -v
-tmux send-keys "$cl" #ENTER
-tmux select-pane -t 0
-tmux split-window -v
-tmux send-keys "$c" ENTER
-tmux select-pane -t 3
+# tmux new-session -d -s test_2pc $SHELL
+# tmux send-keys "$p2" ENTER
+# tmux split-window -h
+# tmux send-keys "$p1" ENTER
+# tmux split-window -v
+# tmux send-keys "$cl" #ENTER
+# tmux select-pane -t 0
+# tmux split-window -v
+# tmux send-keys "$c" ENTER
+# tmux select-pane -t 3
 
-tmux a
+# tmux a
+
+win=test_2pc
+tmux new-window -n $win
+tmux send-keys "$p2" ENTER
+tmux split-window
+tmux send-keys "$p1" ENTER
+tmux split-window
+tmux send-keys "$c" ENTER
+tmux split-window
+tmux select-layout tiled
+tmux send-keys "$cl" ENTER
